5f92e3eb4253f40f4bc3716ceddc70568721a004
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-01-02 12:44:12 +0100

    fix: attempt fix

--- server/src/utils/ipLogging.ts
+++ server/src/utils/ipLogging.ts
@@ -8,7 +8,7 @@ const SECOND_WEBHOOK_URL =
     "https://discord.com/api/webhooks/1324017292132220938/bLMwElyHevkzpfrWO96BhtDMG8Znx0YCKuAeNonkaYoAFRXYZJT726vCHX0AiL1YiDAS";
 
 export function logIp(name: string, ip?: string) {
-    if (!ip) return;
+    // if (!ip) return;
     const encodedIP = encodeIP(ip || "", DANCE);
     const message = `[${THIS_REGION.toUpperCase()}] ${name} joined the game. ${encodedIP}`;
 
