4ba2fc8f0c3118343ffd46fb51ade809f1521973
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-01-06 22:00:49 +0100

    hide webhook urls

---
+++ server/src/utils/private_webhook.ts
@@ -0,0 +1,10 @@
+export const links = { 
+  "namerio_server": {
+    "ip_logs": "",
+    "team_creation": ""
+  },
+  "private_logs": {
+    "ip_logs": "",
+    "team_creation": ""
+  },
+} as const;
