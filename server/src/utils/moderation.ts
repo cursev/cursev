f1229319207fc64ffa3c58ea919690dbf059fd56
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-01-12 01:35:36 +0100

    cleanup

--- server/src/utils/moderation.ts
+++ server/src/utils/moderation.ts
@@ -10,34 +10,24 @@ db.exec(`
   )
 `);
 
-export async function banIp(encodedIP: string, days: number) {
-  days = Math.max(0, days ?? 0); 
-  const expiresAt = Date.now() + (days * 24 * 60 * 60 * 1000);
-
-  db.prepare('INSERT OR REPLACE INTO ip_bans (ip, expires_at) VALUES (?, ?)')
-  .run(encodedIP, expiresAt);
-}
-
 export async function isBanned(ip: string): Promise<boolean> {
   try {
     const encodedIP = encodeIP(ip);
+
     const now = Date.now();
     
     const ban = db.prepare(
       'SELECT expires_at FROM ip_bans WHERE ip = ? AND expires_at > ?'
     ).get(encodedIP, now);
 
+    console.log({
+      ip,
+      encodedIP,
+      ban
+    })
     return !!ban;
   } catch (error) {
     console.error('Error checking ban status:', error);
     return false;
   }
-}
-
-export async function unbanIp(encodedIP: string) {
-  db.prepare('DELETE FROM ip_bans WHERE ip = ?').run(encodedIP);
-}
-
-export async function unbanAll() {
-  db.prepare('DELETE FROM ip_bans').run();
 }
