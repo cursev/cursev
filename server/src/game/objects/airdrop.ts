2a2fb787d7fca731c3cfd7c664feb9f0512f2d14
Anaïs Saraiva <kisakay@mailfence.com>
2025-06-18 03:41:17 +0200

    Update airdrop.ts

--- server/src/game/objects/airdrop.ts
+++ server/src/game/objects/airdrop.ts
@@ -16,8 +16,8 @@ export class AirdropBarn {
     constructor(readonly game: Game) { }
 
     addAirdrop(pos: Vec2, type: string) {
-        const airdrop = new Airdrop(this.game, pos, type);
         if (this.addAirdrop.length < 50) {
+            const airdrop = new Airdrop(this.game, pos, type);
             this.airdrops.push(airdrop);
             this.game.playerBarn.addMapPing("ping_airdrop", pos);
             this.game.objectRegister.register(airdrop);
