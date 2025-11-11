c5969bac56c97ba5dc715eb77a4138792af9e493
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-11-11 13:13:41 +0100

    .

--- shared/defs/maps/baseDefs.ts
+++ shared/defs/maps/baseDefs.ts
@@ -723,7 +723,7 @@ export const Main: MapDef = {
         densitySpawns: [
             {
                 stone_01: 40,
-                barrel_01: 900,
+                barrel_01: 100,
                 silo_01: 1,
                 crate_01: 50,
                 crate_02: 12,
@@ -768,7 +768,7 @@ export const Main: MapDef = {
                 chest_01: 0,
                 chest_03: { odds: 0.2 },
                 mil_crate_02: { odds: 0.25 },
-                tree_02: 100,
+                tree_02: 0,
                 teahouse_complex_01su: {
                     small: 0,
                     large: 2,
